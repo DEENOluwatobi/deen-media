c69b5c9ea327666aaf499c085a89f8467e49307b
DEENOluwatobi <shodimutobie@gmail.com>
2023-02-23 03:21:02 +0100

    msg sidebar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import css from '../styles/Body.module.css'
 import Feed from './Feed'
 import Nav from './Nav'
+import Msg from './Msg'
 import Notification from './Notification'
 
 export default function Body() {
@@ -9,6 +10,7 @@ export default function Body() {
             <Nav/>
             <Feed/>
             <Notification/>
+            <Msg/>
         </div>
     )
 };
---
+++ src/components/Msg.jsx
@@ -0,0 +1,40 @@
+import css from '../styles/Msg.module.css'
+import Image from 'next/image'
+import { UilSearch } from '@iconscout/react-unicons'
+import P5 from '../assets/p5.jpg'
+import P4 from '../assets/p4.jpg'
+import P3 from '../assets/p3.jpg'
+import P2 from '../assets/p2.jpg'
+import P1 from '../assets/p1.jpg'
+
+export default function Msg() {
+    return(
+        <div className={css.msgcon}>
+            <div className={css.search}>
+                <UilSearch/>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P1} alt='' width={50} height={50}/>
+                <div className={css.pbubble}></div>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P2} alt='' width={50} height={50}/>
+                <div className={css.pbubble}></div>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P3} alt='' width={50} height={50}/>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P4} alt='' width={50} height={50}/>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P5} alt='' width={50} height={50}/>
+                <div className={css.pbubble}></div>
+            </div>
+            <div className={css.useractive}>
+                <Image className={css.img} src={P1} alt='' width={50} height={50}/>
+                <div className={css.pbubble}></div>
+            </div>
+        </div>
+    )
+};
